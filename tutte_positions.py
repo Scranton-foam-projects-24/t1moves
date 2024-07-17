d966decd5f12347426e31be4fa26841bd39b916f
ryan-lavelle1 <ryanlavelle0309@gmail.com>
2024-07-17 10:26:30 -0400

    Add files via upload

--- tutte_positions.py
+++ tutte_positions.py
@@ -1,7 +1,6 @@
 import networkx as nx
 import numpy as np
 import copy
-# from main_file import G, pos
 
 def update_neighbors(G,n):
     n_neighbors = list(nx.neighbors(G,n))
@@ -62,21 +61,12 @@ def convert_to_inner_outer(adj_to_io = bool, adj_matrix = list, outer = list): #
         for n in range(len(outer)+1,len(adj_matrix)):
             io_matrix[:,n] = TEMP[:,inner[m]]
             m += 1
-        
-    # else:   #io to adj     not needed, but could maybe be useful
-    #     for n in range(1,len(adj_matrix)):
-    #         io_matrix[n] = adj_matrix[outer[n-1]]
-    #     for n in range(len(outer),len(adj_matrix)):
-    #         io_matrix[n] = adj_matrix[inner[n-1]]
-    
+            
     return io_matrix
 
-def compute_positions_after_tutte(adj_matrix, outer, pos):
-    matrix = laplacian_from_adj(convert_to_inner_outer(True, adj_matrix, outer))
+def compute_positions_after_tutte(matrix, outer, pos):
+
     matrix = np.array(matrix)
-    
-    # print("\n")
-    # print(matrix)
     
     L2 = matrix[len(outer)+1:len(matrix),len(outer)+1:len(matrix)]
     L2_inv = np.linalg.inv(L2)
@@ -85,21 +75,50 @@ def compute_positions_after_tutte(adj_matrix, outer, pos):
     
     P2 = -L2_inv @ B @ P1
     
-    print("\nL2_inv:")
-    print(L2)
-    print("\nB:")
-    print(B)
-    print("\nP1:")
-    print(P1)
-    print("\nP2:")
-    print(P2)
+    # print("\nL2_inv:")
+    # print(L2)
+    # print("\nB:")
+    # print(B)
+    # print("\nP1:")
+    # print(P1)
+    # print("\nP2:")
+    # print(P2)
     # print("\n")
-    
-    # updates pos
-    m = 0
-    for n in range(1,len(pos)):
-        if n not in outer:
-            pos[n] = (P2[m][0],P2[m][1])
-            m += 1
             
     return P2
+
+def update_pos(P2,pos,outer):
+    
+    p2_it = 1
+    outer_it = 0
+    
+    while(p2_it <= len(P2)):
+        if outer_it != len(outer) and p2_it + outer_it == outer[outer_it]:
+            outer_it += 1
+        else:
+            pos[p2_it + outer_it] = P2[p2_it - 1]
+            p2_it += 1
+        
+    # return pos
+
+
+# def update_matrix_and_pos(G,matrix,P2,pos,outer):
+   
+#     matrix = update_matrix(G,matrix)
+#     P2 = compute_positions_after_tutte(matrix, outer, pos)
+#     pos = update_pos(P2,pos,outer)
+    
+#     return matrix, pos
+
+
+def adj_to_io(adj_matrix,outer):
+    
+    adj_matrix = np.array(adj_matrix)
+    result = laplacian_from_adj(adj_matrix)
+    result = convert_to_inner_outer(True, adj_matrix, outer)
+    
+    return result
+
+
+
+
