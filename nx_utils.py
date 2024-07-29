7385de877ecb04e2ac790604d8c3a0455cf8259b
Bernard Scott <bernard.scott@scranton.edu>
2024-07-29 18:34:56 -0400

    Monday work

--- nx_utils.py
+++ nx_utils.py
@@ -4,6 +4,32 @@ import numpy as np
 
 from polygon import Polygon
 
+def has_overlapping_vertices(vertices):
+    """
+    Returns whether the provided list of vertices contains two vertices located
+    at the same position.
+
+    Parameters
+    ----------
+    vertices : list
+        List of array-like objects containing the (x,y) coordinates of each
+        vertex as values.
+
+    Returns
+    -------
+    bool
+        The result of comparing the number of unique x-values with the total
+        number of x-values and the number of unique y-values with the total
+        number of y-values. 
+
+    """
+    for i, vertex1 in enumerate(vertices):
+        for j, vertex2 in enumerate(vertices):
+            if vertex1[0] == vertex2[0] and vertex1[1] == vertex2[1] and i != j:
+                print("CONTAINS DUPLICATES")
+                return True
+    return False
+
 def network_disorder(cells, pos, n=-1):
     """
     Return the network disorder of the network.
@@ -29,7 +55,8 @@ def network_disorder(cells, pos, n=-1):
         n-gon.
 
     """
-    
+    # Try writing the output to a file and seeing what pos is
+    print("pos:", pos)
     poly = Polygon()
     comp_poly = poly.regpoly(n) if n != -1 else None
     
@@ -39,13 +66,18 @@ def network_disorder(cells, pos, n=-1):
         polygon = cells[cell]
         # print(len(polygon))
         vertices = []
-        # num_sides = len(polygon)
-        for i in range(len(polygon)):
+        comp_len = len(polygon)
+        for i in range(comp_len):
+            print("adding:", np.array(pos[polygon[i]]))
             vertices.append(np.array(pos[polygon[i]]))
+            
         print(vertices)
             
+        if has_overlapping_vertices(vertices):
+            continue
+            
         if n == -1:
-            comp_poly = poly.regpoly(len(polygon))
+            comp_poly = poly.regpoly(comp_len)
         # print("calling turning_function.distance()")
         dist, _, _, _ = turning_function.distance(
             vertices, 
@@ -56,5 +88,25 @@ def network_disorder(cells, pos, n=-1):
         turn_dists.append(dist)
 
     return stat.mean(turn_dists)
+
+if __name__ == "__main__":
+    poly = Polygon()
+
+    points = [np.array([0.57608955, 0.5807441 ]), np.array([0.57608955, 0.5807441 ]), np.array([0.57608955, 0.5807441 ])]
+
+    print(has_overlapping_vertices(points))
+
+    comp_poly = poly.regpoly(len(points))
+    
+    # dist, _, _, _ = turning_function.distance(
+    #     points, 
+    #     comp_poly, 
+    #     brute_force_updates=False
+    # )
+
+    poly.graphpoly(points)
+    poly.graphpoly(comp_poly)
+
+    # print(dist)
         
     
--- single_turn_dist.py
+++ single_turn_dist.py
@@ -3,19 +3,46 @@ import numpy as np
 
 from polygon import Polygon
 
+def has_overlapping_vertices(vertices):
+    """
+    Returns whether the provided list of vertices contains two vertices located
+    at the same position.
+
+    Parameters
+    ----------
+    vertices : list
+        List of np.array-like objects containing the (x,y) coordinates of each
+        vertex as values.
+
+    Returns
+    -------
+    bool
+        The result of comparing the number of unique x-values with the total
+        number of x-values and the number of unique y-values with the total
+        number of y-values. 
+
+    """
+    for i, vertex1 in enumerate(vertices):
+        for j, vertex2 in enumerate(vertices):
+            if vertex1[0] == vertex2[0] and vertex1[1] == vertex2[1] and i != j:
+                return True
+    return False
+
 poly = Polygon()
 
-points = [np.array([0.76525199, 0.46502785]), np.array([0.76510414, 0.46327818]), np.array([0.76943905, 0.47132443]), np.array([0.76670732, 0.46963468])]
+points = [np.array([0.57608955, 0.5807441 ]), np.array([0.57608955, 0.5807441 ]), np.array([0.57608955, 0.5807441 ])]
+
+print(has_overlapping_vertices(points))
 
 comp_poly = poly.regpoly(len(points))
 
-dist, _, _, _ = turning_function.distance(
-    points, 
-    comp_poly, 
-    brute_force_updates=False
-)
+# dist, _, _, _ = turning_function.distance(
+#     points, 
+#     comp_poly, 
+#     brute_force_updates=False
+# )
 
 poly.graphpoly(points)
 poly.graphpoly(comp_poly)
 
-print(dist)
+# print(dist)
