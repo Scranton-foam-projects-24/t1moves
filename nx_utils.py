Fixed bug preventing turning distance computation during t1 moves.

nx_utils.py
@@ -1,5 +1,4 @@
 import turning_function
-import statistics as stat
 import numpy as np
 
 from polygon import Polygon
@@ -25,12 +24,12 @@ def has_overlapping_vertices(vertices):
     """
     for i, vertex1 in enumerate(vertices):
         for j, vertex2 in enumerate(vertices):
-            if vertex1[0] == vertex2[0] and vertex1[1] == vertex2[1] and i != j:
-                print("CONTAINS DUPLICATES")
+            # print(f"{round(vertex1[0],7)} == {round(vertex2[0],7)}, {round(vertex1[1],7)} == {round(vertex2[1],7)}, {i} != {j}")
+            if round(vertex1[0],7) == round(vertex2[0],7) and round(vertex1[1],7) == round(vertex2[1],7) and i != j:
                 return True
     return False
 
-def network_disorder(cells, pos, n=-1):
+def network_disorder(cells, pos, n=-1, weighted=False, areas=None):
     """
     Return the network disorder of the network.
 
@@ -46,17 +45,24 @@ def network_disorder(cells, pos, n=-1):
         The number of sides of the regular polygon which every cell will be
         compared to. If -1 (default), every cell will be compared to a k-gon,
         where k is equal to the number of sides of each cell.
+    weighted : bool
+        Optional argument specifying whether the function should weight each
+        turning distance by the area of the cell. Default is False.
+    areas : list
+        Optional argument containing the list of cell areas to be used when
+        weighting computed turning distances. Must be used when weighted=True.
 
     Returns
     -------
-    float
-        A metric quantifying the network disorder of the network, which is the
-        average turning distance of each cell with respect to the specified
-        n-gon.
+    list
+        A list containing the turning distance of each cell with respect to the
+        specified polygon. Default behavior compares each cell to the regular
+        k-gon and returns and does not weight the result by cell area.
 
     """
-    # Try writing the output to a file and seeing what pos is
-    print("pos:", pos)
+    if weighted and areas == None:
+        raise TypeError("Weighted turning distances requires areas.")
+    
     poly = Polygon()
     comp_poly = poly.regpoly(n) if n != -1 else None
     
@@ -64,49 +70,25 @@ def network_disorder(cells, pos, n=-1):
     
     for cell in cells:
         polygon = cells[cell]
-        # print(len(polygon))
+
         vertices = []
-        comp_len = len(polygon)
-        for i in range(comp_len):
-            print("adding:", np.array(pos[polygon[i]]))
+        for i in range(len(polygon)):
             vertices.append(np.array(pos[polygon[i]]))
             
-        print(vertices)
-            
         if has_overlapping_vertices(vertices):
-            continue
-            
-        if n == -1:
-            comp_poly = poly.regpoly(comp_len)
-        # print("calling turning_function.distance()")
-        dist, _, _, _ = turning_function.distance(
-            vertices, 
-            comp_poly, 
-            brute_force_updates=False
-        )
-        # print("computed turning distance")
-        turn_dists.append(dist)
+            turn_dists.append(0)
+        else:
+            if n == -1:
+                comp_poly = poly.regpoly(len(polygon))
+            # print(vertices)
+            dist, _, _, _ = turning_function.distance(
+                vertices, 
+                comp_poly, 
+                brute_force_updates=False
+            )
+            if weighted:
+                turn_dists.append(dist * areas[cell])
+            else:
+                turn_dists.append(dist)
 
-    return stat.mean(turn_dists)
-
-if __name__ == "__main__":
-    poly = Polygon()
-
-    points = [np.array([0.57608955, 0.5807441 ]), np.array([0.57608955, 0.5807441 ]), np.array([0.57608955, 0.5807441 ])]
-
-    print(has_overlapping_vertices(points))
-
-    comp_poly = poly.regpoly(len(points))
-    
-    # dist, _, _, _ = turning_function.distance(
-    #     points, 
-    #     comp_poly, 
-    #     brute_force_updates=False
-    # )
-
-    poly.graphpoly(points)
-    poly.graphpoly(comp_poly)
-
-    # print(dist)
-        
-    
+    return turn_dists

single_turn_dist.py
@@ -11,7 +11,7 @@ def has_overlapping_vertices(vertices):
     Parameters
     ----------
     vertices : list
-        List of np.array-like objects containing the (x,y) coordinates of each
+        List of array-like objects containing the (x,y) coordinates of each
         vertex as values.
 
     Returns
@@ -22,27 +22,41 @@ def has_overlapping_vertices(vertices):
         number of y-values. 
 
     """
+    print("has_overlapping_vertices() called!")
     for i, vertex1 in enumerate(vertices):
         for j, vertex2 in enumerate(vertices):
             if vertex1[0] == vertex2[0] and vertex1[1] == vertex2[1] and i != j:
                 return True
     return False
 
+print("began execution")
 poly = Polygon()
+print("here")
+# points = [np.array([0.07258301, 0.32230874]), np.array([0.07258301, 0.32230874]), np.array([0.07258301, 0.32230874])]
+# points = [np.array([0.92992764, 0.24358688]), np.array([0.90657019, 0.22709718]), np.array([0.90657019, 0.22709718])]
+# points = [np.array([0.11259959, 0.57197477]), np.array([0.11259959, 0.57197477]), np.array([0.11259959, 0.57197477])]
+# points = [np.array([0.8052397 , 0.04203877]), np.array([0.80945053, 0.03633673]), np.array([0.8052397 , 0.04203877])]
+points = [np.array([0.61018154, 0.94410252]), np.array([0.61018154, 0.94410252]), np.array([0.61018154, 0.94410252])]
+# print("here")
+# Things go haywire when two points match
+# EVERYTHING breaks when three points match
 
-points = [np.array([0.57608955, 0.5807441 ]), np.array([0.57608955, 0.5807441 ]), np.array([0.57608955, 0.5807441 ])]
+# pyplot.py runs into an issue before polygon.py can import numpy...
 
 print(has_overlapping_vertices(points))
 
 comp_poly = poly.regpoly(len(points))
 
-# dist, _, _, _ = turning_function.distance(
-#     points, 
-#     comp_poly, 
-#     brute_force_updates=False
-# )
+if not has_overlapping_vertices(points):
+    dist, _, _, _ = turning_function.distance(
+        points, 
+        comp_poly, 
+        brute_force_updates=False
+    )
 
-poly.graphpoly(points)
-poly.graphpoly(comp_poly)
-
-# print(dist)
+    poly.graphpoly(points)
+    poly.graphpoly(comp_poly)
+    
+    print(dist)
+else:
+    print("appended 0")

t1_move_with_gif.py
@@ -352,11 +352,33 @@ def generate_gif(num_per_shot,num_t1,duration):
     log_area = []
     edges = []
     turn_dists = []
+    weighted = True
+    n = 6
     
     while num_t1_in_gif <= num_t1:
         
         print("Recording Network Disorder###")
-        turn_dists.append(nx_utils.network_disorder(cell_major_vertices, pos))
+        areas = compute_cell_areas(pos,cell_major_vertices)
+        if weighted:
+            turn_dists.append(
+                np.sum(nx_utils.network_disorder(
+                    cell_major_vertices, 
+                    pos,
+                    n=n,
+                    weighted=weighted,
+                    areas=areas
+                ))
+            )
+        else:
+            turn_dists.append(
+                np.mean(nx_utils.network_disorder(
+                    cell_major_vertices, 
+                    pos,
+                    n=n,
+                    weighted=weighted,
+                    areas=areas
+                ))
+            )
         
         # snap_title = str("snap"+str(snap_num)+".png")
         
@@ -522,15 +544,10 @@ if __name__ == "__main__":
     periodic = [False,False]
     )
     
-    fig = plt.figure()
-    ax = fig.add_subplot()
-    
     # colorize
     for i, cell in enumerate(cells):    
         polygon = cell['vertices']
         plt.fill(*zip(*polygon),  color = 'black', alpha=0.1)
-        # for i, edge in enumerate(np.array(polygon)):
-        #     ax.annotate(i, edge, xytext=[edge[0]+0.01, edge[1]+0.01])
     
     plt.plot(points[:,0], points[:,1], 'ko')
     plt.xlim(-0.1, 1.1)