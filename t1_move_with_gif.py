1b80445ee65d006417376cc7f4982e85af54707d
ryan-lavelle1 <ryanlavelle0309@gmail.com>
2024-08-26 12:12:30 -0400

    Update t1_move_with_gif.py
    Updated code, updated histogram generation to make them look nicer, minor formatting changes

--- t1_move_with_gif.py
+++ t1_move_with_gif.py
@@ -1,35 +1,22 @@
 import numpy as np
-import scipy.interpolate as interpolate
-import scipy as sp
-import scipy.special as spec
-import scipy.stats as stats
-import csv
-import pylab
 import pyvoro
-
 import networkx as nx
-
-import matplotlib
 import matplotlib.pyplot as plt
-
 import random as rand
-
 from tutte_positions import *
 
 import shutil
 from PIL import Image, ImageFile
 import os
 
-import math
-
 ImageFile.LOAD_TRUNCATED_IMAGES=True
+
 def voro_to_nx():
     
     # [-1,-1] is a dummy entry for ease of working with pos
     major_list = [[-1,-1]]
     local_vertices = [(-1,-1)] # this is (cell_number,local_vertex_number)
                              # index is major vertex number
-    
     H = nx.Graph()
     pos = {}
     outer = []
@@ -39,12 +26,11 @@ def voro_to_nx():
     # fills major_dict
     for m in range(0,len(cells)):   # iterate over each cell
         for n in range(0,len(cells[m]['vertices'])):    # iterate over each vertex
-            temp = [round(cells[m]['vertices'][n][0],7), round(cells[m]['vertices'][n][1],7)]
-            # if cells[m]['vertices'][n].all != major_dict:
+            temp = [round(cells[m]['vertices'][n][0],10), round(cells[m]['vertices'][n][1],10)]
             if temp not in major_list:
-                # major_dict.append(cells[m]['vertices'][n])
                 major_list.append(temp)
                 local_vertices.append((m,n))
+    
     
     # fills pos for outer nodes
     for n in range(1,len(major_list)):
@@ -58,7 +44,7 @@ def voro_to_nx():
             pos[n] = (0,0)
     
     cell_major_vertices, vertices_in_cell = create_cell_major_vertices(H, cells, major_list)
-   
+
     return H, pos, major_list, outer, cell_major_vertices, vertices_in_cell
 
 
@@ -76,11 +62,12 @@ def vnx_add_edges(H, major_list):
             # get node number from major_list
             # add edge between u and v
             if n != len(cells[m]['vertices'])-1:
-                u_pos = [round(cells[m]['vertices'][n][0],7), round(cells[m]['vertices'][n][1],7)]
-                v_pos = [round(cells[m]['vertices'][n+1][0],7), round(cells[m]['vertices'][n+1][1],7)]
+                u_pos = [round(cells[m]['vertices'][n][0],10), round(cells[m]['vertices'][n][1],10)]
+                v_pos = [round(cells[m]['vertices'][n+1][0],10), round(cells[m]['vertices'][n+1][1],10)]
+
             else:
-                u_pos = [round(cells[m]['vertices'][n][0],7), round(cells[m]['vertices'][n][1],7)]
-                v_pos = [round(cells[m]['vertices'][0][0],7), round(cells[m]['vertices'][0][1],7)]
+                u_pos = [round(cells[m]['vertices'][n][0],10), round(cells[m]['vertices'][n][1],10)]
+                v_pos = [round(cells[m]['vertices'][0][0],10), round(cells[m]['vertices'][0][1],10)]
             
             u = major_list.index(u_pos)
             v = major_list.index(v_pos)
@@ -140,10 +127,11 @@ def create_cell_major_vertices(H, cells, major_list):
     
     cell_major_vertices = {}
     vertices_in_cell = {}
-
+        
     for m in range(0,len(cells)): # iterate over every cell
         for n in range(0,len(cells[m]['vertices'])): # iterate over every vertex
-            pos = [round(cells[m]['vertices'][n][0],7), round(cells[m]['vertices'][n][1],7)]
+            pos = [round(cells[m]['vertices'][n][0],10), round(cells[m]['vertices'][n][1],10)]
+            
             if pos in major_list:
                 # Add vertex to cmv
                 if m not in cell_major_vertices:
@@ -160,16 +148,13 @@ def create_cell_major_vertices(H, cells, major_list):
     return cell_major_vertices, vertices_in_cell
     
 
-def do_num_t1_moves(num,num_per_shot):
-
-    interval = math.ceil(num/num_per_shot)
+def do_num_t1_moves(num):
     
-    for m in range(0,interval):
-        for n in range(0,num_per_shot):
-            target_1, target_2, edge_cells, vertex_cells = get_random_t1_targets(cell_major_vertices,vertices_in_cell)
-            do_t1_move(cell_major_vertices,major_list, target_1,target_2, edge_cells,vertex_cells)
+    for n in range(0,num):
+        target_1, target_2, edge_cells, vertex_cells = get_random_t1_targets(cell_major_vertices,vertices_in_cell)
+        do_t1_move(cell_major_vertices,major_list, target_1,target_2, edge_cells,vertex_cells)
             
-        update_P2_and_pos(H, io_matrix, outer, pos)
+    update_P2_and_pos(H, io_matrix, outer, pos)
 
 
 def get_random_t1_targets(cell_major_vertices,vertices_in_cell):
@@ -190,7 +175,6 @@ def get_random_t1_targets(cell_major_vertices,vertices_in_cell):
             idx_2 = 0
             
         target_2 = target_cell[idx_2]
-        
         
         if len(vertices_in_cell[target_1]) < 2:
             valid = False
@@ -233,7 +217,7 @@ def do_t1_move(cell_major_vertices,major_list, target_1,target_2, edge_cells,ver
     
     edge_neighbors(cell_major_vertices, target_1, target_2, edge_cells)
     vertex_neighbors(cell_major_vertices, target_1, target_2, vertex_cells)
-        
+    
     return True
     
 
@@ -278,6 +262,7 @@ def edge_neighbors(cell_major_vertices, target_1, target_2, edge_cells):
         
         io_matrix[io_post][io_lt] = -1
         io_matrix[io_lt][io_post] = -1
+        
         H.add_edge(post,lesser_target)
         
         # edge neighbors lose their CCW "greater" vertex
@@ -325,27 +310,26 @@ def vertex_neighbors(cell_major_vertices, target_1, target_2, vert_cells):
         
         io_matrix[io_prior][io_add] = -1
         io_matrix[io_add][io_prior] = -1
+        
         H.remove_edge(vertex_present,prior)
         
 
 def generate_gif(num_per_shot,num_t1,duration):
     
     # WARNING: WILL DELETE THE TARGET DESTINATION BEFORE EACH RUN
-    # Example for diag_dest - C:\Users\...\Downloads\T1_Moves\diag_snapshots
-    # Recommended: /.../diag, /.../area, etc.
+    
     diag_dest = "./diag/"
     area_dest = "./area/"
     log_area_dest = "./log_area/"
     edge_dest = "./edge/"
-    gif_dest = "./gifs/"
-    dest_list = [diag_dest,area_dest,log_area_dest,edge_dest,gif_dest]
+    dest_list = [diag_dest,area_dest,log_area_dest,edge_dest]
     
     for n in dest_list:
         if os.path.exists(n):
             shutil.rmtree(n)
         os.makedirs(n)
     
-    
+    gif_dest = "./gifs/"
     
     snap_num = 0
     num_t1_in_gif = 0
@@ -353,12 +337,16 @@ def generate_gif(num_per_shot,num_t1,duration):
     area = []
     log_area = []
     edges = []
-    
+
     while num_t1_in_gif <= num_t1:
+        
+        print("Num before saving: "+str(num_t1_in_gif))
         
         snap_title = str("snap"+str(snap_num)+".png")
         
         plt.figure('nx')
+        ax = plt.gca()
+        ax.set_aspect('equal', adjustable='box')
         nx.draw_networkx(H, pos, with_labels=False, node_size = 0)
         plt.savefig(str(diag_dest)+str(snap_title),dpi=200)
         img = Image.open(str(diag_dest)+str(snap_title)) 
@@ -366,62 +354,65 @@ def generate_gif(num_per_shot,num_t1,duration):
         plt.close()
         
         plt.figure('area')
-        plt.savefig(str(area_dest)+str(snap_title))
+        plt.savefig(str(area_dest)+str(snap_title),dpi=200)
         img = Image.open(str(area_dest)+str(snap_title)) 
         area.append(img)
         plt.close()
         
         plt.figure('logarea')
-        plt.savefig(str(log_area_dest)+str(snap_title))
+        plt.savefig(str(log_area_dest)+str(snap_title),dpi=200)
         img = Image.open(str(log_area_dest)+str(snap_title)) 
         log_area.append(img)
         plt.close()
         
         plt.figure('edge')
-        plt.savefig(str(edge_dest)+str(snap_title))
+        plt.savefig(str(edge_dest)+str(snap_title),dpi=200)
         img = Image.open(str(edge_dest)+str(snap_title)) 
         edges.append(img)
         plt.close()
         
         plt.figure('nx')
         print("Doing some t1 moves...")
-        do_num_t1_moves(num_per_shot, num_per_shot)
+        do_num_t1_moves(num_per_shot)
         print("t1 moves done (for now)!")
+        
         num_t1_in_gif += num_per_shot
         
+        print("Doing some area...")
         areas = compute_cell_areas(pos,cell_major_vertices)
-        histogram_area(H,areas)
+        histogram_area(areas)
+        print("Done!")
+        
+        print("Doing some log area...")
         log_areas = compute_cell_log_areas(areas)
-        histogram_log_area(H,log_areas)
-
-        edge = []
-        for n in cell_major_vertices:
-            num_edges= len(cell_major_vertices[n])
-            if num_edges > 20:
-                num_edges = 20
-            edge.append(num_edges)
+        histogram_log_area(log_areas)
+        print("Done!")
+        
+        print("Doing some edges...")
+        edge = compute_cell_edges(cell_major_vertices)
         histogram_edges(H,edge)
+        print("Done!")
         
         snap_num += 1
         
         print("Image "+str(snap_num-1)+"/"+str(int(num_t1/num_per_shot))+" saved!")
-        
+    
     plt.figure('nx')
     diagram[0].save((str(gif_dest)+'diagram.gif'),format='GIF',append_images=diagram[0:],save_all=True,duration=duration,loop=0)
     
     plt.figure('area')
-    area[0].save((str(gif_dest)+'area.gif'),format='GIF',append_images=area[0:],save_all=True,duration=75,loop=0)
+    area[0].save((str(gif_dest)+'area.gif'),format='GIF',append_images=area[0:],save_all=True,duration=125,loop=0)
     
     plt.figure('logarea')
-    area[0].save((str(gif_dest)+'log_area.gif'),format='GIF',append_images=log_area[0:],save_all=True,duration=75,loop=0)
-            
+    area[0].save((str(gif_dest)+'log_area.gif'),format='GIF',append_images=log_area[0:],save_all=True,duration=125,loop=0)
+    
     plt.figure('edge')
-    edges[0].save((str(gif_dest)+'edges.gif'),format='GIF',append_images=edges[0:],save_all=True,duration=75,loop=0)
+    edges[0].save((str(gif_dest)+'edges.gif'),format='GIF',append_images=edges[0:],save_all=True,duration=125,loop=0)
     
     
 def compute_cell_areas(pos,cell_major_vertices):
     
-    areas = []
+    areas_list_in_cca = []
     for m in range(0,len(cell_major_vertices)): # for each cell
         sum_of_determinants = 0
         for n in range(0,len(cell_major_vertices[m])): # for each vertex
@@ -434,50 +425,67 @@ def compute_cell_areas(pos,cell_major_vertices):
             det = ad-bc
             sum_of_determinants += det
             
-        area = .5 * sum_of_determinants
-        if area > .05:
-            area = .05
-        areas.append(area)
+        area_cp = .5 * sum_of_determinants
+
+        areas_list_in_cca.append(area_cp)
+                
+    return areas_list_in_cca
+
+
+def compute_cell_edges(cell_major_vertices):
+    
+    edge = []
+    
+    for n in cell_major_vertices:
+        num_edges = len(cell_major_vertices[n])
+        edge.append(num_edges)
         
-    return areas
+    return edge
 
-def compute_cell_log_areas(areas):
+
+def compute_cell_log_areas(input_areas_list):
     
-    log_areas = []
+    log_areas_list = []
     
-    for n in range(0,len(areas)):
-        log = math.log(areas[n]+1,10)
-        if log > .01:
-            log = .01
-        log_areas.append(log)
+    smallest = 1
+    for n in input_areas_list:
+        if n < smallest:
+            if n > 0:
+                smallest = n
+                
+    for n in input_areas_list:
+        if n < smallest:
+            n = smallest
+        log_of_area = np.log10(n)
 
-    return log_areas
+        log_areas_list.append(log_of_area)
+
+    return log_areas_list
 
 
-def histogram_area(H,data):
+def histogram_area(area_list_here):
     
     plt.figure('area')
     plt.clf()
     
-    plt.hist(data, bins=150, edgecolor='black')
+    n,x,_ = plt.hist(area_list_here, bins=np.arange(-.00025,.02025,.00025), density=True, edgecolor='black', linewidth=1)
+    plt.hist(area_list_here, bins=100,density=True,edgecolor='black', linewidth=1)
+
     plt.title('Area of Cells')
     plt.xlabel('Area')
-    plt.xlim([0,.05])
-    plt.ylabel('Number of Cells')
-    plt.ylim([0,len(data)])
+    plt.ylabel('Density')
     
     
-def histogram_log_area(H,data):
+def histogram_log_area(data_log_areas):
     
     plt.figure('logarea')
     plt.clf()
     
-    plt.hist(data, bins=50, edgecolor='black')
+    n,x,_ = plt.hist(data_log_areas, bins=np.arange(-6,-1.5,.25), density=True, edgecolor='black', linewidth=1)
+
     plt.title('Log of area of Cells')
     plt.xlabel('Log of area')
-    plt.xlim([0,.01])
-    plt.ylabel('Number of Cells')
-    plt.ylim([0,len(data)])
+    plt.ylim([0,1.25])
     
 
 def histogram_edges(H,data):
@@ -485,16 +493,17 @@ def histogram_edges(H,data):
     plt.figure('edge')
     plt.clf()
     
-    plt.hist(data, bins=100, edgecolor='black')
+    n,x,_ = plt.hist(data, bins=np.arange(3,22,1), density=True, edgecolor='black', linewidth=1)
+    
     plt.title('Number of Edges of Cells')
     plt.xlabel('Edges')
-    plt.xlim([3,math.ceil(len(data)/5)])
-    plt.ylabel('Number of Cells')
-    plt.ylim([0,len(data)])
-    
+    plt.xticks(np.arange(3, 21, step=2))
+    plt.ylim([0,0.5])
+
+
     
 if __name__ == "__main__":
-    
+        
     print("Generating Voronoi diagram...")
     plt.figure('voro')
     np.random.seed([1938430])
@@ -502,15 +511,14 @@ if __name__ == "__main__":
     # generates 10 "random" lists with 2 elements, over [0,1)
     # also picks colors
     
-    dots_num = 499
+    dots_num = 2499
     
     colors = np.random.rand(dots_num, 3) 
     points = np.random.rand(dots_num, 2)
-     
     
     # make color map (unnecessary for just random colorization)
     color_map = {tuple(coords):color for coords, color in zip(points, colors)}
-     
+    
     cells = pyvoro.compute_2d_voronoi(
     points, # point positions, 2D vectors this time.
     [[0.0, 1.0], [0.0, 1.0]], # box size
@@ -526,8 +534,6 @@ if __name__ == "__main__":
     plt.plot(points[:,0], points[:,1], 'ko')
     plt.xlim(-0.1, 1.1)
     plt.ylim(-0.1, 1.1)
-    
-    # plt.show()
     
     print("Voronoi diagram created!")
     
@@ -546,14 +552,14 @@ if __name__ == "__main__":
     io_matrix = convert_to_inner_outer(True, laplacian, outer)
     print("io_matrix populated!")
     
-    
-    num_t1 = 5000000
-    # do_num_t1_moves(num_t1)
      
+    
     # To manually select targets for t1 moves, use the following:
     # do_t1_move(cell_major_vertices,major_list, target_1,target_2, edge_cells,vertex_cells)
     
     plt.figure('nx')
+    # print("Drawing nx...")
+    # nx.draw_networkx(H, pos, with_labels=True, node_size = 15)
     # Draw graph with vertex labels
     # nx.draw_networkx(H, pos, with_labels=True, node_size = 15)
     
@@ -562,32 +568,49 @@ if __name__ == "__main__":
     print("Update complete!")
     
     # Draw graph without vertex labels
-    # nx.draw_networkx(H, pos, with_labels=False, node_size = 0)
-    areas = compute_cell_areas(pos,cell_major_vertices)
+    print("Setting ax...")
+    ax = plt.gca()
+    ax.set_aspect('equal', adjustable='box')
+    print("Ax done!")
+    print("Computing areas...")
+    areas_init = compute_cell_areas(pos,cell_major_vertices)
     plt.figure('area')
-    histogram_area(H,areas)
+    histogram_area(areas_init)
+    print("Done computing areas!")
     
     plt.figure('logarea')
-    log_areas = compute_cell_log_areas(areas)
-    histogram_log_area(H,log_areas)
+    log_areas = compute_cell_log_areas(areas_init)
+    histogram_log_area(log_areas)
+    print("Done!")
     
+    print("Doing some edges...")
     plt.figure('edge')
-    edge = []
-    for n in cell_major_vertices:
-        num_edges= len(cell_major_vertices[n])
-        if num_edges > 20:
-            num_edges
-        edge.append(num_edges)
-            
-    histogram_edges(H,edge)
     
-    snapshot_interval = 100
+    edge = compute_cell_edges(cell_major_vertices)
+    histogram_edges(H,edge)
+    print("Done doing edges!")
+    
+    
+    
+    num_t1 = 30000
+    
+    # To manually select targets for t1 moves, use the following:
+    # do_t1_move(cell_major_vertices,major_list, target_1,target_2, edge_cells,vertex_cells)
+    
+    # If you do not want a gif, uncomment this line and comment out 'generate_gif(...)'
+    # do_num_t1_moves(num_t1)
+    
+    # How many T1 moves happen between snapshots in the GIF
+    snapshot_interval = 10000
+    
     print("Beginning GIF generation...")
     generate_gif(snapshot_interval,num_t1,duration=.1)
     print("GIF generation done!")
     
     plt.figure('nx')
+    ax = plt.gca()
+    ax.set_aspect('equal', adjustable='box')
+    nx.draw_networkx(H, pos, with_labels=False, node_size = 0)
+    plt.savefig('./final_fig.png',dpi=200)
     
-    # nx.draw_networkx(H, pos, with_labels=False, node_size = 0)
-    # nx.draw_networkx(H, pos, with_labels=True, node_size = 15)
-    # update_everything(H,laplacian,outer,pos)
+    
